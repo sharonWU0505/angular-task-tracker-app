ed53ac02f78689d15b596a26aff7e3eae69adb35
sharonWU0505 <yasyuanwu@gmail.com>
2021-08-30 15:46:51 +0800

    feat: add delete and toggle features

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
@@ -11,8 +11,18 @@ export class TaskItemComponent implements OnInit {
   faTimes = faTimes;
 
   @Input() task: Task;
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter();
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter();
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  onDelete(task: Task): void {
+    this.onDeleteTask.emit(task);
+  }
+
+  onToggle(task: Task): void {
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -13,6 +13,18 @@ export class TasksComponent implements OnInit {
   constructor(private taskService: TasksService) {}
 
   ngOnInit(): void {
+    this.getTask();
+  }
+
+  getTask(): void {
     this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
+
+  deleteTask(task: Task): void {
+    this.taskService.deleteTask(task).subscribe(() => this.getTask());
+  }
+
+  toggleTaskReminder(task: Task): void {
+    this.taskService.toggleTask(task).subscribe(() => this.getTask());
   }
 }
